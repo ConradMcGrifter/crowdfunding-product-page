0e1097b5669d66f6b4004c0b20766617ae34b0ec
C <conradmcgrifter@protonmail.com>
2021-05-30 02:27:06 -0400

    i feel like i should start over. i started this project without planning

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let body = document.querySelector("body");
 let xButton = document.getElementById("x-btn");
 
 let radio1 = document.getElementById("radio-1");
+let radio2 = document.getElementById("radio-2");
+let radio3 = document.getElementById("radio-3");
 
 callToActionBtn.addEventListener("click", openCloseProjectCard)
 callToActionBtn.addEventListener("click", scrollIntoView)
@@ -31,8 +33,8 @@ radio1.addEventListener("click", ()=> {
     
     if (radio1.checked == true) {
         document.querySelector(".no-reward-pledge").style.border = "solid hsl(176, 72%, 28%) 2px";
-    } else if (radio1.checked == false) {
-        document.querySelector(".no-reward-pledge").style.border = "solid rgba(161, 161, 161, 0.295) 2px;";
+    } else if (radio2.checked == true) {
+        document.querySelector(".no-reward-pledge").style.border = "solid hsl(176, 72%, 28%) 2px";
     }
 })
 
